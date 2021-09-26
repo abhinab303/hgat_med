a09910e98f2e0e13746d5a02473488a00919e446
Abhinab <070bel303@pcampus.edu.np>
2021-09-26 19:00:34 +0545

    data read

--- code/preprocess.py
+++ code/preprocess.py
@@ -1,1 +1,8 @@
-data_dir = ""
+data_dir = "/content/drive/MyDrive/hgat_med/hgat_med/data"
+data_path = "/content/drive/MyDrive/hgat_med/hgat_med/data/2021MergedEnglish.csv"
+
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv(data_path)
+print(df.head())
